0c866bdb85959c2e618c17378d6352e796d6f3be
ZephR55 <bekiraydemir55@gmail.com>
2023-09-21 16:25:10 +0300

    initial commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,8 +54,8 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     //Retrofit Implementation
-    //implementation ("'com.squareup.retrofit2:retrofit:2.6.2'")
-    //implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation ("com.squareup.retrofit2:retrofit:2.6.2")
+    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
 
 // Kotlin
     implementation("androidx.navigation:navigation-fragment-ktx:$nav_version")
